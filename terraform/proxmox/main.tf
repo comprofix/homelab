7b90c446a5e129e317a3ee72684bb6d59671898c
Matthew McKinnon <mmckinnon@comprofix.com>
2024-05-23 21:43:40 +1000

    feat(proxmox): added docker terraform

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -2,89 +2,12 @@
 # ---
 # Create a new VM from a clone
 
-resource "proxmox_vm_qemu" "gitlab" {
-    
-    # VM General Settings
-    target_node = "pve1"
-    vmid = "600"
-    name = "Gitlabtf"
-    desc = "Gitlab Server"
-
-
-    # VM Advanced General Settings
-    onboot = true
-    bootdisk = "scsi0"
-    scsihw       = "virtio-scsi-single"
-
-    # VM OS Settings
-    clone = "debian-12-generic-amd64"
-    clone_wait        = 45
-    timeouts {
-        create = "20m"
-        delete = "10m"
-    }
-
-    # VM System Settings
-    agent = 1
-    
-    # VM CPU Settings
-    cores = 1
-    sockets = 4
-    cpu = "kvm64"
-    bios = "ovmf"
-
-    
-    # VM Memory Settings
-    memory = 16384
-
-    # VM Network Settings
-    network {
-        bridge = "vmbr0"
-        model  = "virtio"
-        tag    = "10"
-    }
-
-    efidisk {
-        efitype = "4m"
-        storage = "storage0"
-    }
-
-    disks {
-    scsi {
-      scsi0 {
-        disk {
-          size       = 80
-          storage    = "storage0"
-        }
-      }
-    }
-  }
-
-    # VM Cloud-Init Settings
-    os_type = "cloud-init"
-    cloudinit_cdrom_storage = "storage0"
-
-    # (Optional) IP Address and Gateway
-    ipconfig0 = "ip=10.10.10.99/24,gw=10.10.10.1"
-    nameserver = "10.10.10.1"
-    
-    # (Optional) Default User
-    ciuser = "${var.ci_user}"
-    cipassword = "${var.ci_password}"
-    
-    # # (Optional) Add your SSH Public KEY
-    sshkeys = <<EOF
-    ${ var.ssh_key }
-    EOF
-
-}
-
 resource "proxmox_vm_qemu" "docker" {
     
     # VM General Settings
     target_node = "pve1"
-    vmid = "601"
-    name = "Dockertf"
+    vmid = "105"
+    name = "docker"
     desc = "Docker Server"
 
 
@@ -123,7 +46,7 @@ resource "proxmox_vm_qemu" "docker" {
 
     efidisk {
         efitype = "4m"
-        storage = "storage0"
+        storage = "proxmox"
     }
 
     disks {
@@ -131,7 +54,7 @@ resource "proxmox_vm_qemu" "docker" {
       scsi0 {
         disk {
           size       = 40
-          storage    = "storage0"
+          storage    = "proxmox"
         }
       }
     }
@@ -139,10 +62,10 @@ resource "proxmox_vm_qemu" "docker" {
 
     # VM Cloud-Init Settings
     os_type = "cloud-init"
-    cloudinit_cdrom_storage = "storage0"
+    cloudinit_cdrom_storage = "proxmox"
 
     # (Optional) IP Address and Gateway
-    ipconfig0 = "ip=10.10.10.100/24,gw=10.10.10.1"
+    ipconfig0 = "ip=10.10.10.5/24,gw=10.10.10.1"
     nameserver = "10.10.10.1"
     
     # (Optional) Default User
