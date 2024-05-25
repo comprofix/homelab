9c84c53912306a2b576f004e7a7be3edd5c349b0
Matthew McKinnon <mmckinnon@comprofix.com>
2024-05-26 00:02:48 +1000

    chore(scripts): fix deploy script for gitlab-ci.yml

--- ansible/scripts/deploy.sh
+++ ansible/scripts/deploy.sh
@@ -7,11 +7,16 @@ if [ ! -z "$changed_tasks" ]; then
         basename=$(basename "$task")
         role=$(echo "$task" | awk -F/ '{print $3}')
         role_task=$(echo "$task" | awk -F/ '{print $4}')
-        tags=${basename%.*}_install
+        
         if [[ "$basename" != "deploy-homelab.yml" && "$basename" != "all.yml" && "$basename" != "all.example.yml" && "$basename" != ".gitlab-ci.yml" && "$role_task" != "defaults" && "$role_task" != "meta" && "$role_task" != "vars" && "$role_task" != "templates" ]] ; then
-            #if [[ "$role" = "containers" ]]; then
+            if [[ "$role" = "containers" ]]; then
+                tags=${basename%.*}_install
                 ansible-playbook deploy-homelab.yml --tags "$tags" --vault-password-file ~/.vault_password.txt
-            #fi
+            else
+                tags=${role%.*}_install
+                ansible-playbook deploy-homelab.yml --tags "$tags" --vault-password-file ~/.vault_password.txt
+            fi
+
         fi
     done
 else
