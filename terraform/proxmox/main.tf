56c6d020d988fdc216ec4f297efc0cfae0b95150
Matthew McKinnon <mmckinnon@comprofix.com>
2024-05-25 11:13:17 +1000

    fix(terraform): Fixed Terraform Promox state

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -13,7 +13,6 @@ resource "proxmox_vm_qemu" "docker" {
 
     # VM Advanced General Settings
     onboot = true
-    bootdisk = "scsi0"
     scsihw       = "virtio-scsi-single"
 
     # VM OS Settings
@@ -26,6 +25,8 @@ resource "proxmox_vm_qemu" "docker" {
 
     # VM System Settings
     agent = 1
+    machine = "q35"
+    qemu_os = "l26"
     
     # VM CPU Settings
     cores = 1
@@ -66,6 +67,85 @@ resource "proxmox_vm_qemu" "docker" {
 
     # (Optional) IP Address and Gateway
     ipconfig0 = "ip=10.10.10.5/24,gw=10.10.10.1"
+    nameserver = "10.10.10.1"
+    
+    # (Optional) Default User
+    ciuser = "${var.ci_user}"
+    cipassword = "${var.ci_password}"
+    
+    # # (Optional) Add your SSH Public KEY
+    sshkeys = <<EOF
+    ${ var.ssh_key }
+    EOF
+
+}
+
+
+resource "proxmox_vm_qemu" "gitlab" {
+    
+    # VM General Settings
+    target_node = "pve1"
+    vmid = "100"
+    name = "gitlab"
+    desc = "Gitlab Server"
+
+
+    # VM Advanced General Settings
+    onboot = true
+    scsihw       = "virtio-scsi-single"
+
+    # VM OS Settings
+    clone = "debian-12-generic-amd64"
+    clone_wait        = 45
+    timeouts {
+        create = "20m"
+        delete = "10m"
+    }
+
+    # VM System Settings
+    agent = 1
+    machine = "q35"
+    qemu_os = "l26"
+    
+    # VM CPU Settings
+    cores = 1
+    sockets = 4
+    cpu = "kvm64"
+    bios = "ovmf"
+
+    
+    # VM Memory Settings
+    memory = 16384
+
+    # VM Network Settings
+    network {
+        bridge = "vmbr0"
+        model  = "virtio"
+        tag    = "10"
+    }
+
+    efidisk {
+        efitype = "4m"
+        storage = "proxmox"
+    }
+
+    disks {
+    scsi {
+      scsi0 {
+        disk {
+          size       = 80
+          storage    = "proxmox"
+        }
+      }
+    }
+  }
+
+    # VM Cloud-Init Settings
+    os_type = "cloud-init"
+    cloudinit_cdrom_storage = "proxmox"
+
+    # (Optional) IP Address and Gateway
+    ipconfig0 = "ip=10.10.10.2/24,gw=10.10.10.1"
     nameserver = "10.10.10.1"
     
     # (Optional) Default User
