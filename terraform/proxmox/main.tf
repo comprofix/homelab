12636488b1191fd6a821b5c58527d9c26d09310f
Matthew McKinnon <mmckinnon@comprofix.com>
2024-06-08 17:36:24 +1000

    feat(terraform): Add docker-dev machine

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -159,6 +159,83 @@ resource "proxmox_vm_qemu" "gitlab" {
 
 }
 
+resource "proxmox_vm_qemu" "gitea" {
+    
+    # VM General Settings
+    target_node = "pve2"
+    vmid = "103"
+    name = "gitea"
+  
+
+    # VM Advanced General Settings
+    onboot = true
+    scsihw       = "virtio-scsi-single"
+
+    # VM OS Settings
+    clone = "debian-12-generic-amd64"
+    clone_wait        = 45
+    timeouts {
+        create = "20m"
+        delete = "10m"
+    }
+
+    # VM System Settings
+    agent = 1
+    machine = "q35"
+    qemu_os = "l26"
+    
+    # VM CPU Settings
+    cores = 1
+    sockets = 4
+    cpu = "kvm64"
+    bios = "ovmf"
+
+    
+    # VM Memory Settings
+    memory = 16384
+
+    # VM Network Settings
+    network {
+        bridge = "vmbr0"
+        model  = "virtio"
+        tag    = "10"
+    }
+
+    efidisk {
+        efitype = "4m"
+        storage = "local-zfs"
+    }
+
+    disks {
+    scsi {
+      scsi0 {
+        disk {
+          size       = 40
+          storage    = "local-zfs"
+        }
+      }
+    }
+  }
+
+    # VM Cloud-Init Settings
+    os_type = "cloud-init"
+    cloudinit_cdrom_storage = "local-zfs"
+
+    # (Optional) IP Address and Gateway
+    ipconfig0 = "ip=10.10.10.8/24,gw=10.10.10.1"
+    nameserver = "10.10.10.1"
+    
+    # (Optional) Default User
+    ciuser = "${var.ci_user}"
+    cipassword = "${var.ci_password}"
+    
+    # # (Optional) Add your SSH Public KEY
+    sshkeys = <<EOF
+    ${ var.ssh_key }
+    EOF
+
+}
+
 
 resource "proxmox_lxc" "omada" {
     
@@ -171,6 +248,8 @@ resource "proxmox_lxc" "omada" {
 
     password = "${var.ci_password}"
 
+    memory = 4096
+    swap = 512
 
 
     # VM Advanced General Settings
@@ -182,10 +261,7 @@ resource "proxmox_lxc" "omada" {
         delete = "10m"
     }
   
-    # VM Memory Settings
-    memory = 4096
-    cores = 2
-    swap = 512
+    # VM Memory Settingsport 2019
 
     unprivileged = true
     features {
@@ -209,6 +285,83 @@ resource "proxmox_lxc" "omada" {
     
     # # (Optional) Add your SSH Public KEY
     ssh_public_keys = <<EOF
+    ${ var.ssh_key }
+    EOF
+
+}
+
+resource "proxmox_vm_qemu" "docker-dev" {
+    
+    # VM General Settings
+    target_node = "pve2"
+    vmid = "2000"
+    name = "docker-dev"
+
+    # VM Advanced General Settings
+    onboot = true
+    scsihw       = "virtio-scsi-single"
+
+    # VM OS Settings
+    clone = "debian-12-generic-amd64"
+    clone_wait        = 45
+    
+    timeouts {
+        create = "20m"
+        delete = "10m"
+    }
+
+    # VM System Settings
+    agent = 1
+    machine = "q35"
+    qemu_os = "l26"
+    
+    # VM CPU Settings
+    cores = 1
+    sockets = 4
+    cpu = "kvm64"
+    bios = "ovmf"
+
+    
+    # VM Memory Settings
+    memory = 8192
+
+    # VM Network Settings
+    network {
+        bridge = "vmbr0"
+        model  = "virtio"
+        tag    = "10"
+    }
+
+    efidisk {
+        efitype = "4m"
+        storage = "local-zfs"
+    }
+
+    disks {
+    scsi {
+      scsi0 {
+        disk {
+          size       = 40
+          storage    = "local-zfs"
+        }
+      }
+    }
+  }
+
+    # VM Cloud-Init Settings
+    os_type = "cloud-init"
+    cloudinit_cdrom_storage = "local-zfs"
+
+    # (Optional) IP Address and Gateway
+    ipconfig0 = "ip=10.10.10.9/24,gw=10.10.10.1"
+    nameserver = "10.10.10.1"
+    
+    # (Optional) Default User
+    ciuser = "${var.ci_user}"
+    cipassword = "${var.ci_password}"
+    
+    # # (Optional) Add your SSH Public KEY
+    sshkeys = <<EOF
     ${ var.ssh_key }
     EOF
 
--- terraform/proxmox/provider.tf
+++ terraform/proxmox/provider.tf
@@ -44,7 +44,7 @@ provider "proxmox" {
     pm_api_token_id = var.proxmox_api_token_id
     pm_api_token_secret = var.proxmox_api_token_secret
     pm_timeout = 1800
-    pm_parallel = 2 # Fix vm hdd lock timeout
+    #pm_parallel = 2 # Fix vm hdd lock timeout
 
     # (Optional) Skip TLS Verification
     # pm_tls_insecure = true
