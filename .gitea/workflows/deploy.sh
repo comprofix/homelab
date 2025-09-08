12444311a4780c50a2625af434dd9d230a68ed5f
Matthew McKinnon <mmckinnon@comprofix.com>
2025-09-08 18:29:40 +1000

    Initial Commit

---
+++ .gitea/workflows/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+changed_tasks=($(git diff --name-only $1 $2 | grep '\.yml$'))
+if [ ! -z "$changed_tasks" ]; then
+    for task in "${changed_tasks[@]}"; do
+        tag=$(echo "$task" | awk -F/ '{print $2}')
+        if [[ "$tag" != "deploy-homelab.yml" && "$tag" != "main.yml" && "$tag" != "all.yml" && "$tag" != "all.example.yml" && "$tag" != "ISSUE_TEMPLATE" && "$tag" != "workflows"   ]] ; then
+           tag=${tag%.*}_install
+           ansible-playbook main.yml --tags "$tag" --vault-password-file ~/.vault_password.txt          
+        fi
+    done
+else
+    echo "No changes detected in task files. Skipping Ansible playbook execution."
+fi
---
+++ vault.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# If number of arguments is 0
+if [ $# -eq 0 ]
+  then
+    echo "This script will encrypt of decrypt all files containing secrets."
+    echo "There are all files in vars as well as all secrets.yaml files under each service."
+    echo "Specify 'decrypt' or 'encrypt' as argument"
+    echo "If you put the vault password in a password file named .vault_password, the script will not ask for a password."
+    exit 1
+fi
+
+#files=`find . \( -type d -name 'group_vars' -o -name 'vars' \) -exec find {} -type f \;`
+files=(
+  "./group_vars/all.yml"
+  "./roles/docker/vars/main.yml"
+  "./roles/traefik/vars/main.yml"
+  "./scripts/rclone.conf"
+)
+
+# password_type=--ask-vault-password
+# if [ -f "~/.vault_password.txt" ]
+#   then
+#     if [ `stat -c %a ~/.vault_password.txt` != "600" ]
+#       then
+#         echo "~/.vault_password.txt file has bad permissions; fixing this to 600"
+#         chmod 600 ~/.vault_password.txt
+#     fi
+#     password_type="--vault-password-file=~/.vault_password.txt"
+# fi
+
+if [ $1 == "encrypt" ]
+  then
+  for file in "${files[@]}"; do
+    echo "$file encrypted" 
+    ansible-vault encrypt --vault-password-file=~/.vault_password "$file"
+    echo $value;
+  done
+
+elif [ $1 == "decrypt" ]
+  then
+  for file in "${files[@]}"; do
+    echo "$file decrypted" 
+    ansible-vault decrypt --vault-password-file=~/.vault_password "$file"
+  done
+else
+    echo "Wrong argument supplied.  Run without arguments to see allowed ones."
+fi
