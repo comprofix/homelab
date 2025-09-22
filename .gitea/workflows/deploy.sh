dfd0d13f830675aa7baaa1afa565b1851837eabb
Matthew McKinnon <mmckinnon@comprofix.com>
2025-09-22 19:50:37 +1000

    chore: updated workflow for docker role

--- .gitea/workflows/deploy.sh
+++ .gitea/workflows/deploy.sh
@@ -5,6 +5,7 @@ if [ ! -z "$changed_tasks" ]; then
         tag=$(echo "$task" | awk -F/ '{print $2}')
         if [[ "$tag" != "deploy-homelab.yml" && "$tag" != "main.yml" && "$tag" != "all.yml" && "$tag" != "all.example.yml" && "$tag" != "ISSUE_TEMPLATE" && "$tag" != "workflows"   ]] ; then
            tag=${tag%.*}_install
+           ansible-galaxy install -r requirements.yml
            ansible-playbook main.yml --tags "$tag" --vault-password-file ~/.vault_password.txt          
         fi
     done
